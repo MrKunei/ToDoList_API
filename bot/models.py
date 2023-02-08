ca0c46ec7ea70bfb1684c3ea03a947d5d914452c
Vladimir <vladimir.kunei@gmail.com>
2023-02-08 13:35:45 +0300

    update ci & bot

--- bot/models.py
+++ bot/models.py
@@ -1,11 +1,18 @@
+import os
 from django.db import models
-
 from core.models import User
+
 
 
 class TgUser(models.Model):
     tg_id = models.BigIntegerField(unique=True)
     tg_chat_id = models.BigIntegerField()
-    user = models.ForeignKey(User, models.PROTECT, null=True, blank=True)
+    username = models.CharField(max_length=255, null=True, blank=True, default=None)
+    user = models.ForeignKey(User, models.PROTECT, null=True, blank=True, default=None)
 
-    verification_code = models.CharField(max_length=32, null=True, blank=True)
+
+    verification_code = models.CharField(max_length=32, null=True, blank=True, default=None)
+
+    def set_verification_code(self):
+        code = os.urandom(12).hex()
+        self.verification_code = code
--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("oauth/", include("social_django.urls", namespace="social")),
     path('core/', include('core.urls')),
     path('goals/', include('goals.urls')),
+    path('bot/', include('bot.urls')),
+
 ]
